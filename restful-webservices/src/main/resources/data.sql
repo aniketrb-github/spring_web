1a38541e8723367051fc718ce1af4318dee9938a
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-16 01:01:32 +0530

    added SQL scripts for post entity

--- restful-webservices/src/main/resources/data.sql
+++ restful-webservices/src/main/resources/data.sql
@@ -1,3 +1,14 @@
 insert into user values(100, sysdate(), 'Adama Traore');
 insert into user values(101, sysdate(), 'James Daniel');
 insert into user values(102, sysdate(), 'Brendon Williams');
+
+insert into post values(2000, 'Traore: The Frightening Lightening!', 100);
+insert into post values(2001, 'Traore: Thrashes ManCity to oblivion in FA Cup', 100);
+
+insert into post values(2002, 'Brendon Williams shines as the replacement of Luke Shaw', 102);
+insert into post values(2003, 'Brendon Williams, the new speedster left back at ManUnited', 102);
+
+insert into post values(2004, 'DanJames scores on his debut against Chelsea at Old Trafford', 101);
+insert into post values(2005, 'DanJames the new kid becomming a sensational signing of Ole Gunnar Solskjaer', 101);
+
+

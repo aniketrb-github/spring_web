97358060fb6cf8db534e208d6ecf879c82ec4f93
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-15 16:34:03 +0530

    added enhanced swagger annotations for details and descriptions

--- restful-webservices/src/main/java/com/arbtech/configuration/SwaggerConfig.java
+++ restful-webservices/src/main/java/com/arbtech/configuration/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.arbtech.configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,10 +19,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("", "", "");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("API  Title", "API Description", "v1.0", "termsOfServiceUrl",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+	  
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
 	// Created a Docket bean which will map all API's & end-points into the swagger UI in browser
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 	
 	/**
--- restful-webservices/src/main/java/com/arbtech/model/User.java
+++ restful-webservices/src/main/java/com/arbtech/model/User.java
@@ -5,15 +5,24 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * User model class
  * 
  * @author Aniket Bharsakale
  */
+@ApiModel(description = "The User Model/POJO used in the application.")
 public class User {
 	@Size(min = 2, message = "Name should be more than 2 characters")
+	@ApiModelProperty(notes = "Name should be more than 2 characters")
 	private String name;
+	
+	@ApiModelProperty(notes = "The auto-generated identifier by system for an user.")
 	private Integer id;
+
+	@ApiModelProperty(notes = "Birthdate cannot be a future/present date.")
 	@Past(message = "Birthdate cannot be a future/present date.")
 	private Date birthDate;
 
